b6e05c567005058fd6353c9af3963e3ead4a07e6
Neil Mistry <neil@amplience.com>
2020-07-27 10:52:50 +0100

    feat: Dev box endpoint update

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
     env: {
-        contentApi: 'gaptest.cdn.content.amplience.net'
+        contentApi: 'gaptestdev.cdn.content.amplience.net'
     },
     poweredByHeader: false,
     target: 'serverless'
