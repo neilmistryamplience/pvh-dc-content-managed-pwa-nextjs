94f9aec353873fe57975209e792cbb66b83b475b
Neil Mistry <neil@amplience.com>
2020-08-07 13:58:03 +0100

    Updates for WTC NL Account

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
     env: {
-        contentApi: 'gaptestdev.cdn.content.amplience.net'
+        contentApi: 'salmonnl.cdn.content.amplience.net'
     },
     poweredByHeader: false,
     target: 'serverless'
