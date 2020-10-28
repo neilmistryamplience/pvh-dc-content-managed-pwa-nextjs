e1eefa5d9accb9873e18a1e856f87c4ca974d601
Neil Mistry <neil@amplience.com>
2020-10-28 12:46:34 +0000

    PVH: Updates from TA

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
     env: {
-        contentApi: 'salmonnl.cdn.content.amplience.net'
+        contentApi: 'thomdemo.cdn.content.amplience.net'
     },
     poweredByHeader: false,
     target: 'serverless'
